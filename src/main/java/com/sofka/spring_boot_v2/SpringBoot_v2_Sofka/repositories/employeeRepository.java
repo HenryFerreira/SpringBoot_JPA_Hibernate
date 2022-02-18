4f5187d03aebe74c1d2ef1f64a1f0db9eb091348
HenryFerreira <henryferreira133@gmail.com>
2022-02-18 20:50:24 -0300

    Uso de TEST para probar el H2

--- src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/repositories/employeeRepository.java
+++ src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/repositories/employeeRepository.java
@@ -1,5 +1,13 @@
 package com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.repositories;
 
-public interface employeeRepository {
+import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.models.employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository//--> Indica que esta Interfaz es un Repositorio
+public interface employeeRepository extends JpaRepository<employee, Long> {//--> Indica que es una tabla de tipo
+    // 'employee' y que su identificador(PrimaryKey) será de tipo Long
+    employee findByEmployeeId(String employeeId);//--> SELECT * FROM employee WHERE employeeId = '[parametro enviado]';
+    employee findByFirstName(String firstName);//--> SELECT * FROM employee WHERE employeeId = '[parametro enviado]';
+    employee findByLastName(String lastName);//--> SELECT * FROM employee WHERE employeeId = '[parametro enviado]';
 }
---
+++ src/test/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/employeeJpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.sofka.spring_boot_v2.SpringBoot_v2_Sofka;
+
+import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.models.employee;
+import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.repositories.employeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest//--> Indica que este TEST se va a utilizar solamente para JPA
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class employeeJpaRepositoryTest {
+    @Autowired
+    private employeeRepository empRepository;
+
+    @Test
+    public void saveEmployee(){
+        employee jhon = new employee("Jhon", "Smith", "empl123");
+        employee claire = new employee("claire", "Simpson", "empl124");
+
+        empRepository.save(jhon);
+        empRepository.save(claire);
+
+        empRepository.flush();
+
+        assertEquals(2, empRepository.findAll().size());
+    }
+}
