717ed7db88d25a64cb07d8dfe8bdd58cd846150e
HenryFerreira <henryferreira133@gmail.com>
2022-02-18 13:38:21 -0300

    Se refactorizo el nombre de las variables y de las clases

--- src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/models/employee.java
+++ src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/models/employee.java
@@ -13,14 +13,14 @@ public class employee {
     private Long id;
 
     @Column(length = 25, nullable = false)//--> Indica que es una columna con longitud de 25 caracteres y no nula
-    private String first_name;
+    private String firstName;
 
     @Column(length = 25, nullable = false)//--> Indica que es una columna con longitud de 25 caracteres y no nula
-    private String last_name;
+    private String lastName;
 
     @Column(length = 10, nullable = false, unique = true)/*--> Indica que es una columna con longitud de 10 caracteres,
      no nula y unica*/
-    private String employee_id;
+    private String employeeId;
     //----------------------------------------------------------------------------------------------------------------//
 
     //----------------------------------------------------------------------------------------------------------------//
@@ -29,10 +29,10 @@ public class employee {
 
     }
 
-    public employee(String first_name, String last_name, String employee_id) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.employee_id = employee_id;
+    public employee(String firstName, String lastName, String employeeId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeId = employeeId;
     }
     //----------------------------------------------------------------------------------------------------------------//
 
@@ -46,28 +46,28 @@ public class employee {
         this.id = id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public String getEmployee_id() {
-        return employee_id;
+    public String getEmployeeId() {
+        return employeeId;
     }
 
-    public void setEmployee_id(String employee_id) {
-        this.employee_id = employee_id;
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
     }
     //----------------------------------------------------------------------------------------------------------------//
 
@@ -90,9 +90,9 @@ public class employee {
     public String toString() {//Devuelve el objeto en formato JSON
         return "employee{" +
                 "id=" + id +
-                ", first_name='" + first_name + '\'' +
-                ", last_name='" + last_name + '\'' +
-                ", employee_id='" + employee_id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", employeeId='" + employeeId + '\'' +
                 '}';
     }
     //----------------------------------------------------------------------------------------------------------------//
---
+++ src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/repositories/employeeRepository.java
@@ -0,0 +1,5 @@
+package com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.repositories;
+
+public interface employeeRepository {
+
+}
