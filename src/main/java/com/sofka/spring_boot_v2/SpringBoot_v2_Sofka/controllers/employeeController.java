9e91c782be96509c9a271172547cf275f9cd2cf8
HenryFerreira <henryferreira133@gmail.com>
2022-02-20 17:34:23 -0300

    Se implementaron los metodos 'getEmployee', 'getEmployeeByName', 'updateEmployeeById', 'deleteEmployeeById' en el Controlador y en su respectivo Servicio

--- src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/controllers/employeeController.java
+++ src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/controllers/employeeController.java
@@ -3,9 +3,13 @@ package com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.controllers;
 import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.models.employee;
 import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.services.employeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RestController//--> Indica que es un controlador
 @RequestMapping("/employee")//Indica en que direccion del servidor se activan los metodos de esta clase
 public class employeeController {
@@ -13,14 +17,39 @@ public class employeeController {
     employeeService empService = new employeeService();
 
     @GetMapping()
-    public ArrayList<employee> getAllEmployee(){
+    public ArrayList<employee> getAllEmployee() {
         return this.empService.getAllEmployee();
     }
 
+    @GetMapping("/search/id/{id}")
+    public Optional<employee> getEmployee(@PathVariable("id") Long id) {
+        return this.empService.getEmployeeById(id);
+    }
+
+    @GetMapping("/search/name/{firstName}")
+    public List<employee> getEmployeeByName(@PathVariable("firstName") String name) {
+        return this.empService.getEmployeeByName(name);
+    }
+
     @PostMapping()
-    public employee addNewEmployee(@RequestBody employee newEmploye){
+    public employee addNewEmployee(@RequestBody employee newEmploye) {
         System.out.println(newEmploye);
         return this.empService.addNewEmployee(newEmploye);
     }
+
+    @PutMapping("/update/id/{id}")
+    public ResponseEntity<employee> updateEmployeeById(@PathVariable("id") Long id, @RequestBody employee employeeData) {
+        return this.empService.updateEmployeeById(id, employeeData);
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public String deleteEmployeeById (@PathVariable("id") Long id){
+        if(this.empService.deleteEmployeeById(id)){//Si devuelve true
+            return "Se elimino el Empleado con ID: [" + id + "]";
+        } else {// Si devuelve false
+            return "No pudo eliminar el Empleado con ID: [" + id + "]";
+        }
+    }
+
 
 }
--- src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/services/employeeService.java
+++ src/main/java/com/sofka/spring_boot_v2/SpringBoot_v2_Sofka/services/employeeService.java
@@ -3,9 +3,13 @@ package com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.services;
 import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.models.employee;
 import com.sofka.spring_boot_v2.SpringBoot_v2_Sofka.repositories.employeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 //2. Creación de los servicios de un CRUD. ( Crear, Eliminar, Actualizar, Obtener todos y Obtener por ID). 30%
 @Service
@@ -13,12 +17,46 @@ public class employeeService {
     @Autowired
     employeeRepository empRepository;
 
-    public ArrayList<employee> getAllEmployee(){//Obtener todos los empleados
+    public Optional<employee> getEmployeeById(Long id){
+        return this.empRepository.findById(id);
+    }
+
+    public List<employee> getEmployeeByName(String name){
+        return this.empRepository.findByFirstName(name);
+    }
+
+    public ArrayList<employee> getAllEmployee() {//Obtener todos los empleados
         return (ArrayList<employee>) empRepository.findAll();
     }
 
-    public employee addNewEmployee(employee newEmployee){//Agregar un nuevo empleado
+    public employee addNewEmployee(employee newEmployee) {//Agregar un nuevo empleado
         return empRepository.save(newEmployee);
+    }
+
+    public ResponseEntity<employee> updateEmployeeById(Long id, employee employeeData) {
+        Optional<employee> updatedEmployee = this.empRepository.findById(id);
+
+        if (updatedEmployee.isPresent()) {
+            employee _employee = updatedEmployee.get();
+            _employee.setFirstName(employeeData.getFirstName());
+            _employee.setLastName(employeeData.getLastName());
+            _employee.setEmployeeId(employeeData.getEmployeeId());
+            _employee.set_role(employeeData.get_role());
+            _employee.setProjects(employeeData.getProjects());
+            
+            return new ResponseEntity<>(empRepository.save(_employee), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public boolean deleteEmployeeById(Long id) {
+        try {
+            this.empRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 
